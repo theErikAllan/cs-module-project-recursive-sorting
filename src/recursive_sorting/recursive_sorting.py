e8306e0c189aee8bf76607096b388f22a116fa9d
Rick Ahlgren <soul@Ricks-MBP.attlocal.net>
2020-05-19 19:16:35 -0400

    Spun in circles

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -4,7 +4,30 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here 
-    # Initial commit
+
+    # First, we create two variables to represent our traversal through the indices of arrA and arrB
+    a = 0
+    b = 0
+
+    # Next, we write a for loop that will run as many times as there are elements in the (final) merged array
+    for c in range(0, elements):
+        # Compare a and b
+        # If a is out of range, we push b into merged array and iterate
+        if a >= len(arrA):
+            merged_arr[c] = arrB[b]
+            b += 1
+        # if b is out of range, we push a into merged array and iterate
+        elif b >= len(arrB):
+            merged_arr[c] = arrA[a]
+            a += 1
+        # if a smaller, we put it in the merged array and iterate both
+        elif arrA[a] < arrB[b]:
+            merged_arr[c] = arrA[a]
+            a += 1
+        # if b is smaller, we put it in the merged array and iterate both
+        elif arrB[b] < arrA[a]:
+            merged_arr[c] = arrB[b]
+            b += 1
 
 
     return merged_arr
@@ -13,9 +36,11 @@ def merge(arrA, arrB):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # Your code here
-
-
-    return arr
+    if len(arr) > 1:
+        left = merge_sort(arr[0 : len(arr) // 2])
+        right = merge_sort(arr[len(arr) // 2 : ])
+        merged_arr = merge(left, right)
+    return merged_arr
 
 
 # implement an in-place merge sort algorithm
